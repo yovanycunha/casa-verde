3fe9b9ab8cd85ff85b903454b579bdad493fafdd
Yovany Cunha <yovany33@gmail.com>
2022-10-04 21:15:23 -0300

    save progress

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -1,3 +1,23 @@
+import style from './Newsletter.module.scss';
+
 export default function Newsletter() {
-  return <div>NEWSLETTER</div>;
+  return (
+    <section className={style.container}>
+      <div className={style.headerWrapper}>
+        <h1>
+          Sua casa com as
+          <strong>melhores plantas</strong>
+        </h1>
+        <p>
+          Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
+          torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail
+          e assine nossa newsletter para saber das novidades da marca.
+        </p>
+      </div>
+      <div className={style.backgroundsWrapper}>
+        <div className={style.vector} />
+        <div className={style.plant} />
+      </div>
+    </section>
+  );
 }
